a5a6d1ec3635b6ac5eb5bb8347a940691a8f4049
cgtinker <Denys.Hsu@gmail.com>
2023-04-05 12:21:48 +0200

    slight docker opt

--- dockercpghpgs.sh
+++ dockercpghpgs.sh
@@ -2,6 +2,11 @@ echo Attempting to copy pages from container
 
 # save .git file from deletion
 cp $PWD/build/.git $PWD/.git.tmp
-docker cp ghpg:/src/build $PWD/build
+docker exec ghpg npm run predeploy
+
+# move files if docker shell isn't -it
+# docker cp ghpg:/src/build $PWD/build
+
 mv $PWD/.git.tmp $PWD/build/.git
+
 
